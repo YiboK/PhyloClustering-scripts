24da47d03b2b513206e2c9017e1a3fb7fd995309
Yibo Kong <k825379114@gmail.com>
2023-12-20 15:49:42 -0600

    redid nni plots, move k-means-investigate.ipynb

--- R/dog-data-plot-tree.R
+++ R/dog-data-plot-tree.R
@@ -49,14 +49,14 @@ write.tree(trees, file='./dog-data/sample_200k_rooted.trees')
 
 trees <- read.tree("../../baobabs/kmeans_1.trees")
 for (i in 1:length(trees)){
-  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Adi001"))
+  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Smi165"))
 }
 write.tree(trees, file="../../baobabs/kmeans_rooted_1.trees") 
 
 trees <- read.tree("../../baobabs/kmeans_2.trees")
 for (i in 1:length(trees)){
   
-  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Adi001"))
+  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Smi165"))
 }
 write.tree(trees, file="../../baobabs/kmeans_rooted_2.trees") 
 
@@ -64,7 +64,7 @@ write.tree(trees, file="../../baobabs/kmeans_rooted_2.trees")
 trees <- read.tree("../../baobabs/hc_1.trees")
 for (i in 1:length(trees)){
   
-  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Adi001"))
+  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Smi165"))
 }
 write.tree(trees, file="../../baobabs/hc_rooted_1.trees") 
 
@@ -72,7 +72,7 @@ write.tree(trees, file="../../baobabs/hc_rooted_1.trees")
 trees <- read.tree("../../baobabs/hc_2.trees")
 for (i in 1:length(trees)){
   
-  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Adi001"))
+  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Smi165"))
 }
 write.tree(trees, file="../../baobabs/hc_rooted_2.trees") 
 
@@ -80,6 +80,6 @@ write.tree(trees, file="../../baobabs/hc_rooted_2.trees")
 trees <- read.tree("../../baobabs/trees.trees")
 for (i in 1:length(trees)){
   
-  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Adi001"))
+  trees[[i]] = reroot(trees[[i]], which(trees[[i]]$tip.label=="Smi165"))
 }
 write.tree(trees, file="../../baobabs/trees.trees") 
